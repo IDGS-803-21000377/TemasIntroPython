75fa385adbbe4210e4f1d9b4374c55eaa129c08a
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-01-14 14:58:36 -0600

    cambios

--- ejercicio.py
+++ ejercicio.py
@@ -1,16 +1,19 @@
 
-x = 0
-n=1
+n = 1
 
 print("dame un numero ")
 num = input('Numero: ')
-res = int(num)+int(n)
+res = int(num)
 
 
 if res == 0:
     print("todo numero multiplicado por 0 es 0 ")
 else :
-    while res < 10:
-        print(res)
+    while n <=10:
+        result = res * n
+
+        print("la multiplicacion de {} * {} ={}".format(num,n,result))
+
+        n+=1
     
   
